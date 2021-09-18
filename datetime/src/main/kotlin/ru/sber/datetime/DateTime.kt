9c689630a0d11ce5644bbaa55abf6c9b5c4f5eaa
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-09-18 21:37:57 +0300

    Версия_1

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -4,22 +4,57 @@ import java.time.LocalDateTime
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val allZones: Set<String> = ZoneId.getAvailableZoneIds()
+    val dt: LocalDateTime = LocalDateTime.now()
+
+    val resultSet: MutableSet<String> = mutableSetOf()
+
+    val zoneList: List<String> = ArrayList<String>(allZones)
+    sort(zoneList)
+
+    for (s in zoneList) {
+        val zone: ZoneId = ZoneId.of(s)
+        val zdt: ZonedDateTime = dt.atZone(zone)
+        val offset: ZoneOffset = zdt.offset
+        val secondsOfHours: Int = offset.totalSeconds % (60 * 60)
+
+        if (secondsOfHours != 0) {
+            resultSet.add(zone.toString())
+        }
+    }
+    return resultSet
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    val lastInMonthDayWeekList: MutableList<String> = mutableListOf()
+
+    for (i in 1..12) {
+        val yearMonth: YearMonth = YearMonth.of(year, i)
+        val lastDay: LocalDate = yearMonth.atEndOfMonth()
+        lastInMonthDayWeekList.add(lastDay.dayOfWeek.toString())
+    }
+    return lastInMonthDayWeekList
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    var counter: Int = 0
+    for (i in 1..12) {
+        val yearMonth: YearMonth = YearMonth.of(year, i)
+        val thirteenthDay: LocalDate = yearMonth.atDay(13)
+        if (thirteenthDay.dayOfWeek == DayOfWeek.FRIDAY) {
+            counter++
+        }
+
+    }
+
+    return counter
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm"))
 }
 
 
