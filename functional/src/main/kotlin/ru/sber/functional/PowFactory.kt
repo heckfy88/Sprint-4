27b76e32d39fac4df4b123c9d4042d6ab8fe916e
Ruslan Agaev <ruslanagaev@list.ru>
2021-09-21 00:48:57 +0300

    Functional_done_v1

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,17 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int): (Int) -> Int {
+        return { valueToPow -> valueToPow.toDouble().pow(pow).toInt()}
     }
+}
+
+
+fun main() {
+    print(PowFactory.buildPowFunction(2)(100))
 }
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,55 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun initStudents(students: List<Student>) {
+        this.students = students
     }
+
+    fun filterByPredicate(condition: (Student) -> Boolean): List<Student> {
+        if (this::students.isInitialized) {
+            return students.filter(condition)
+        }
+        return emptyList()
+     }
+}
+
+fun main() {
+    val a = StudentsGroup()
+    a.initStudents(
+        listOf(
+            Student(
+                firstName = "John",
+                lastName = "Doe",
+                averageRate = 2.3,
+                age = 0,
+                city = "No city mentioned",
+                specialization = "No specialization mentioned",
+                prevEducation = "No previous education",
+                middleName = "No middle name"
+            ),
+            Student(
+                firstName = "John",
+                lastName = "Doea",
+                averageRate = 4.0,
+                age = 10,
+                city = "No city mentioned",
+                specialization = "No specialization mentioned",
+                prevEducation = "No previous education",
+                middleName = "No middle name"
+            ),
+            Student(
+                firstName = "Johna1",
+                lastName = "Doea232",
+                averageRate = 4.0,
+                age = 20,
+                city = "No city mentioned",
+                specialization = "No specialization mentioned",
+                prevEducation = "No previous education",
+                middleName = "No middle name"
+            ),
+        )
+    )
+
+    println(a.filterByPredicate { condition -> condition.firstName == "John" && condition.lastName == "Doe" })
+
 }
